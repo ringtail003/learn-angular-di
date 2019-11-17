1ac2d69838f8e461b5a02f93b53cc38c78093139
ringtail003 <ringtail003@gmail.com>
2019-11-17 15:33:44 +0900

    some-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SomeComponent } from './component/some/some.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SomeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/component/some/some.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'some',
+  templateUrl: './some.component.html',
+  styleUrls: ['./some.component.scss']
+})
+export class SomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
